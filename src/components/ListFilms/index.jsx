cf3f0fac242808381ab66e4059e56046297c71d1
gabrielalencs <gabrieldealencar.dev@gmail.com>
2024-09-05 20:48:54 -0300

    feat: makes logic for rescuing films in theaters

--- src/components/ListFilms/index.jsx
+++ src/components/ListFilms/index.jsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from 'react';
+
 import '../../styles/layout/listFilms.scss';
 
 import Image from '../../assets/images/image-4.png';
@@ -6,45 +8,75 @@ import { FaStar } from "react-icons/fa6";
 
 
 const ListFilms = () => {
+
+    const [favoriteMoviesList, setFavoriteMoviesList] = useState([]);
+
+
+    useEffect(() => {
+
+        const getFavoriteMoviesList = async () => {
+
+            const data = await fetch('https://api.themoviedb.org/3/movie/now_playing?api_key=071bb306893009d6309f4184450837f3&language=pt-BR');
+
+            const response = await data.json();
+
+            setFavoriteMoviesList(response.results);
+
+        };
+
+        getFavoriteMoviesList();
+
+    }, [])
+
+
+
     return (
         <section className='listFilmsContainer container'>
-            <div className='filmContainer'>
-                <div className='filmContainerImage'>
-                    <img src='https://image.tmdb.org/t/p/w500/qzyUeoCg6fh8nSQD1LFiUiiOs8P.jpg' />
-                </div>
 
-                <div className='filmContainerMainTitle'>
-                    <span className='mainTitle'>
-                        Por água abaixo
-                    </span>
-                    <div className='containerStars'>
-                        <FaStar />
-                        <FaStar />
-                        <FaStar />
-                        <FaStar />
-                        <FaStar />
+
+            {
+                favoriteMoviesList.map(favoriteMovie => (
+                    <div className='filmContainer' key={favoriteMovie.id}>
+                        <div className='filmContainerImage'>
+                            <img src={`https://image.tmdb.org/t/p/w500/${favoriteMovie.poster_path}`} />
+                        </div>
+
+                        <div className='filmContainerMainTitle'>
+                            <span className='mainTitle'>
+                                {favoriteMovie.title}
+                            </span>
+                            <div className='containerStars'>
+                                <FaStar />
+                                <FaStar />
+                                <FaStar />
+                                <FaStar />
+                                <FaStar />
+                            </div>
+                        </div>
+
+                        <div className='filmContainerMoreInformation'>
+                            <span className='mainTitle'>
+                                {favoriteMovie.title}
+                            </span>
+                            <div className='containerStars'>
+                                <FaStar />
+                                <FaStar />
+                                <FaStar />
+                                <FaStar />
+                                <FaStar />
+                            </div>
+                            <p className='moreInformationDescription'>
+                                {favoriteMovie.overview}
+                            </p>
+                            <button className='btnSeeMore'>Ver Mais</button>
+                        </div>
+
+                        <div className='filmContainerBackdrop'></div>
                     </div>
-                </div>
+                ))
+            }
 
-                <div className='filmContainerMoreInformation'>
-                    <span className='mainTitle'>
-                        Por água abaixo
-                    </span>
-                    <div className='containerStars'>
-                        <FaStar />
-                        <FaStar />
-                        <FaStar />
-                        <FaStar />
-                        <FaStar />
-                    </div>
-                    <p className='moreInformationDescription'>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores perspiciatis assumenda facilis molestias nostrum dicta hic cum fugiat tenetur veniam itaque modi non, voluptas officiis est mollitia reiciendis. Quisquam, tempore.
-                    </p>
-                    <button className='btnSeeMore'>Ver Mais</button>
-                </div>
 
-                <div className='filmContainerBackdrop'></div>
-            </div>
         </section>
     )
 }
