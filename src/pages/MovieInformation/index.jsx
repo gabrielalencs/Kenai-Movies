b7931fcd916b2c502686d6974eba32b18d79b640
gabrielalencs <gabrieldealencar.dev@gmail.com>
2024-09-12 19:11:55 -0300

    feat: ends movie information component header

--- src/pages/MovieInformation/index.jsx
+++ src/pages/MovieInformation/index.jsx
@@ -1,45 +1,58 @@
-import React from 'react'
+// Styles
+
+import '../../styles/layout/movieInformation.scss';
+
+// React Router
+
 import { useLocation } from 'react-router-dom';
+
 
 const MovieInformation = () => {
 
-    const location = useLocation();
-    const movieInformation = location.state.infoMovie;
+    // const location = useLocation();
+    // const movieInformation = location.state.infoMovie;
 
-    console.log(movieInformation);
-
+    // console.log(movieInformation);
 
 
     return (
         <section className='movieInformationContainer'>
-            <div className='movieInformationImage'>
-                <img src="https://image.tmdb.org/t/p/w400/xq4v7JE8niZ75OYYPDGNn6Gzpyt.jpg" alt="poster do filme" />
-            </div>
+            <div className='movieInformationHeader'>
+                <div className="container">
+                    <div className='movieInformationImage'>
+                        <img src="https://image.tmdb.org/t/p/w400/xq4v7JE8niZ75OYYPDGNn6Gzpyt.jpg" alt="poster do filme" />
+                    </div>
 
-            <div>
-                <div>
-                    <h3>Deadpool & Wolverine</h3>
-                    <span>coraçãozinho</span>
-                </div>
+                    <div className='movieInformationMainInfos'>
+                        <div className='movieTitle'>
+                            <h3>Deadpool & Wolverine</h3>
+                            {/* <span>coraçãozinho</span> */}
+                        </div>
 
-                <div>
-                    <p>Data de Lançamento <span>24/07/2024</span></p>
-                    <p>Duração: <span>2h 8m</span></p>
-                </div>
+                        <div className='movieOtherInformation'>
+                            <p>Data de Lançamento: <span>24/07/2024</span></p>
+                            <p>Duração: <span>2h 8m</span></p>
+                            <p>Avaliação dos Usuários: <span>6.294%</span></p>
+                        </div>
 
-                <div>
-                    <span>Gênero do Filme: </span>
-                    <ul>
-                        <li>Ação</li>
-                        <li>Comédia</li>
-                        <li>Ficção científica</li>
-                    </ul>
-                </div>
+                        <div className='movieGenre'>
+                            <p>
+                                Gênero do Filme:
+                                <ul>
+                                    <li>Ação</li>
+                                    <li>Comédia</li>
+                                    <li>Ficção científica</li>
+                                </ul>
+                            </p>
+                        </div>
 
-                <div>
-                    <p>
-                        Um apático Wade Wilson trabalha duro na vida civil. Seus dias como mercenário moralmente flexível, Deadpool, ficou para trás. Quando seu planeta enfrenta uma ameaça, Wade deve relutantemente vestir-se novamente com um ainda mais relutante... relutante? Mais relutante? Ele deve convencer um Wolverine.
-                    </p>
+                        <div className='movieSynopsis'>
+                            <h4>Sinopse</h4>
+                            <p>
+                                Um apático Wade Wilson trabalha duro na vida civil. Seus dias como mercenário moralmente flexível, Deadpool, ficou para trás. Quando seu planeta enfrenta uma ameaça, Wade deve relutantemente vestir-se novamente com um ainda mais relutante... relutante? Mais relutante? Ele deve convencer um Wolverine.
+                            </p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </section>
